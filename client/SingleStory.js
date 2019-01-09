094965d7c647df6e784c56e3e9109fb1332661b0
FCraven <craven.francis@gmail.com>
2019-01-09 16:39:45 -0500

    dummy data in SingleStory changed to an axios call for data

--- client/SingleStory.js
+++ client/SingleStory.js
@@ -1,58 +1,48 @@
 import React, { Component } from 'react'
-
-const fakeStory = {
-  title: 'Ships',
-  id: 2,
-  content: "A ship in port is safe,\nbut that's not what ships are built for",
-  author: {
-    id: 1,
-    name: 'Grace Hopper'
-  },
-  comments: [
-    {
-      id: 4,
-      content: 'I like ships!',
-      author: {
-        id: 2,
-        name: 'Alan Turing',
-        imageUrl: 'default-author.png'
-      }
-    }
-  ]
-}
+import axios from 'axios'
 
 export default class SingleStory extends Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      story: fakeStory
+      story: {
+        comments: [],
+        content: ''
+      }
     }
+  }
+
+  async componentDidMount() {
+    const { storyId } = this.props.match.params
+    const res = await axios.get(`api/stories/${storyId}`)
+    const story = res.data
+    this.setState({ story })
   }
 
   render() {
     const story = this.state.story
-    const content = story.content ||  ''
+    const content = story.content
     const comments = story.comments
     console.log(this.props)
     return (
       <div id='single-story' className='column'>
         <h1>{story.title}</h1>
         {
-          content.split('\n').map((line,idx) => <p key={idx}>{line}</p>)
+          content.split('\n').map((line, idx) => <p key={idx}>{line}</p>)
         }
         <h3>Responses:</h3>
         {
           comments.map(comment => (
-            <div className='comment row' key ={comment.id}>
-             <img src={comment.author.imageUrl} />
-             <div className='column'>
+            <div className='comment row' key={comment.id}>
+              <img src={comment.author.imageUrl} />
+              <div className='column'>
                 <a>
                   <h5>{comment.author.name}</h5>
                 </a>
                 <div>{comment.content}</div>
+              </div>
             </div>
-          </div>
           ))
         }
       </div>
