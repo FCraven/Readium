30486aa5177badbe1b2f4407930985d1bb5b8241
FCraven <craven.francis@gmail.com>
2019-01-10 23:13:42 -0500

    added SingleAuthor component

---
+++ client/SingleAuthor.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react'
+
+export default class SingleAuthor extends Component {
+  render() {
+    return (
+      <div id='single-author' className='column'>
+        <div id='sinle-author-detail' className='row'>
+          <div className='column mr1'>
+            <h1>AUTHOR_NAME</h1>
+            <p>AUTHOR_BIO</p>
+          </div>
+          <img src='AUTHOR-IMAGEURL' />
+        </div>
+        <hr/>
+        <div>
+          <h4>STORIES</h4>
+          <h4>COMMENTS</h4>
+        </div>
+      </div>
+    )
+  }
+}
