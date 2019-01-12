a329590968798499fb2a3a59347c0b4740816ccc
FCraven <craven.francis@gmail.com>
2019-01-11 19:17:34 -0500

    end

--- client/Main.js
+++ client/Main.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from './Navbar'
 import AllStories from './AllStories'
-import {HashRouter as Router, Route} from 'react-router-dom'
+import {HashRouter as Router, Route, Switch} from 'react-router-dom'
 import SingleStory from './SingleStory'
 import Authors from './Authors'
 import SingleAuthor from './SingleAuthor'
@@ -17,12 +17,12 @@ export default class Main extends React.Component {
             </div>
             <Navbar />
           </div>
-          <Route exact path='/' component={AllStories} />
-          <Route exact path='/stories' component={AllStories} />
-          <Route path='/stories/:storyId' component={SingleStory} />
-          <Route exact path='/authors' component={Authors} />
-          <Route path='/authors/:authorId' component={SingleAuthor} />
-        </div>
+            <Route exact path='/' component={AllStories} />
+            <Route exact path='/stories' component={AllStories} />
+            <Route path='/stories/:storyId' component={SingleStory} />
+            <Route exact path='/authors' component={Authors} />
+            <Route path='/authors/:authorId' component={SingleAuthor} />
+         </div>
       </Router>
     )
   }
