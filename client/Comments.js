7ca49ad41c61959524f7ed62f48332d70d77d779
FCraven <craven.francis@gmail.com>
2019-01-11 19:21:54 -0500

    added Link to img tag in commetns to provide navigation to comment author

--- client/Comments.js
+++ client/Comments.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const Comments = (props) => props.comments.map(comment => (
       <div className='comment row' key={comment.id}>
-        <img src={comment.author.imageUrl} />
+        <Link to={`/authors/${comment.author.id}`}>
+          <img src={comment.author.imageUrl} />
+        </Link>
         <div className='column'>
           <a>
             <h5>{comment.author.name}</h5>
